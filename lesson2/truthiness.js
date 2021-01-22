df0506e0d531e884e590f099d5e639d39db85da8
Mike Barry <mbarry@packetdriving.com>
2021-01-22 03:30:10 -0600

    ran truthiness through linter

--- lesson2/truthiness.js
+++ lesson2/truthiness.js
@@ -15,7 +15,7 @@ makeLonger("abc", true);
 makeLonger("xyz", false);
 
 function isDigit(char) {
-  if (char >= "0" && char <="9") {
+  if (char >= "0" && char <= "9") {
     return true;
   } else {
     return false;
@@ -32,7 +32,7 @@ if (value === true) {
 } else if (value === false) {
   console.log("It's false!");
 } else {
-  console.log("It's not true or false!")
+  console.log("It's not true or false!");
 }
 
 let num = 5;
@@ -42,7 +42,7 @@ if (num < 10) {
 } else {
   console.log("large number");
 }
-  
+
 function isSmall(number) {
   return number < 10;
 }
@@ -55,27 +55,27 @@ if (isSmall(num)) {
   console.log("Large Number");
 }
 
-let lastName
+let lastName;
 
 if (lastName && (lastName.length > 0)) {
-  console.log(`Hi, ${lastName}.`)
+  console.log(`Hi, ${lastName}.`);
 } else {
-  console.log("Hello, whoever you are.")
+  console.log("Hello, whoever you are.");
 }
 
 // JavaScript evaluates almost all values as true except for:
 // - false
 // - undefined
 // - null
-// - 0 
+// - 0
 // - "" (an empty string)
 // - NaN
 
 num = 5;
 if (num) {
   console.log("valid number");
-} else { 
-  console.log("error!")
+} else {
+  console.log("error!");
 }
 
 console.log(num === true);
